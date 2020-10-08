dd67505818c7e2477b14a811a9dc55ebda749018
Andrew Mayorov <a.mayorov@rbkmoney.com>
2020-10-08 15:51:25 +0300

    Sprinkle few explanatory comments

--- proto/auth_context_provider.thrift
+++ proto/auth_context_provider.thrift
@@ -6,8 +6,19 @@ include "proto/context.thrift"
 
 exception UserNotFound {}
 
+/**
+ * Сервис, предоставляющий bouncer-контексты для задач авторизации.
+ */
 service AuthContextProvider {
 
+    /**
+     * Получить контекст пользователя по его идентификатору.
+     *
+     * Предполагается, что контекст будет содержать информацию о пользователе, которую можно
+     * уместить в [`context_v1.User`][1].
+     *
+     * [1]: https://github.com/rbkmoney/bouncer-proto/blob/97dcad6f/proto/context_v1.thrift#L78
+     */
     context.ContextFragment GetUserContext (1: domain.UserID id) throws (
         1: UserNotFound ex1
     )
